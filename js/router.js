6f3afc865603a2dbcb2ec5075cd46695de9da8e4
Riccardo <Riccardo@DESKTOP-F6RRVKD>
2016-05-17 21:49:18 +0200

    transition

--- js/router.js
+++ js/router.js
@@ -2,100 +2,102 @@
 var router = {
     parseRule: function (rule, modState) {
         var myRule = $(rule).data('request');
-        switch (myRule.rule) {
+        if (myRule !== null) {
+            switch (myRule.rule) {
 
-            case 'home' :
-                if (modState === true)
-                    history.replaceState({"rule": "home"}, null, '#!home');
-                $('#menu').addClass('visibile');
-                break;
+                case 'home' :
+                    if (modState === true)
+                        history.replaceState({"rule": "home"}, null, '#!home');
+                    $('#menu').addClass('visibile');
+                    break;
 
-            case 'contatti' :
-                if (modState === true)
-                    history.pushState(myRule, null, '#!contatti');
-                $("#container-all-section").load("./contatti.html");
-                switchMenu('contatti');
-                $('#menu').removeClass('visibile');
-                break;
+                case 'contatti' :
+                    if (modState === true)
+                        history.pushState(myRule, null, '#!contatti');
+                    $("#container-all-section").load("./contatti.html");
+                    switchMenu('contatti');
+                    $('#menu').removeClass('visibile');
+                    break;
 
-            case 'news' :
-                if (modState === true)
-                    history.pushState(myRule, null, '#!news');
-                $("#container-all-section").data("params", myRule);
-                $("#container-all-section").load('list-standard.html');
-                switchMenu('news');
-                $('#menu').removeClass('visibile');
-                break;
+                case 'news' :
+                    if (modState === true)
+                        history.pushState(myRule, null, '#!news');
+                    $("#container-all-section").data("params", myRule);
+                    $("#container-all-section").load('list-standard.html');
+                    switchMenu('news');
+                    $('#menu').removeClass('visibile');
+                    break;
 
-            case 'vangelo' :
-                if (modState === true)
-                    history.pushState(myRule, null, '#!vangelo');
-                $("#container-all-section").data("params", myRule);
-                $("#container-all-section").load('list-standard.html');
-                switchMenu('vangelo');
-                $('#menu').removeClass('visibile');
-                break;
+                case 'vangelo' :
+                    if (modState === true)
+                        history.pushState(myRule, null, '#!vangelo');
+                    $("#container-all-section").data("params", myRule);
+                    $("#container-all-section").load('list-standard.html');
+                    switchMenu('vangelo');
+                    $('#menu').removeClass('visibile');
+                    break;
 
-            case 'agenda' :
-                if (modState === true)
-                    history.pushState(myRule, null, '#!agenda');
-                $("#container-all-section").data("params", myRule);
-                $("#container-all-section").load('list-agenda.html');
-                switchMenu('agenda');
-                $('#menu').removeClass('visibile');
-                break;
+                case 'agenda' :
+                    if (modState === true)
+                        history.pushState(myRule, null, '#!agenda');
+                    $("#container-all-section").data("params", myRule);
+                    $("#container-all-section").load('list-agenda.html');
+                    switchMenu('agenda');
+                    $('#menu').removeClass('visibile');
+                    break;
 
-            case 'gmg' :
-                if (modState === true)
-                    history.pushState(myRule, null, '#!gmg');
-                $("#container-all-section").data("params", myRule);
-                $("#container-all-section").load('page-gmg.html');
-                switchMenu('gmg');
-                $('#menu').removeClass('visibile');
-                break;
+                case 'gmg' :
+                    if (modState === true)
+                        history.pushState(myRule, null, '#!gmg');
+                    $("#container-all-section").data("params", myRule);
+                    $("#container-all-section").load('page-gmg.html');
+                    switchMenu('gmg');
+                    $('#menu').removeClass('visibile');
+                    break;
 
-            case 'single' :
-                if (modState === true)
-                    history.pushState(myRule, null, '#!single');
-                $("#container-all-section").data("params", myRule);
-                $("#container-all-section").load('single.html');
-                switchMenu(myRule.cat);
-                $('#menu').removeClass('visibile');
-                break;
+                case 'single' :
+                    if (modState === true)
+                        history.pushState(myRule, null, '#!single');
+                    $("#container-all-section").data("params", myRule);
+                    $("#container-all-section").load('single.html');
+                    switchMenu(myRule.cat);
+                    $('#menu').removeClass('visibile');
+                    break;
 
-            case 'album' :
-                if (modState === true)
-                    history.pushState(myRule, null, '#!album');
-                $("#container-all-section").data("params", myRule);
-                $("#container-all-section").load('list-album.html');
-                switchMenu('album');
-                $('#menu').removeClass('visibile');
-                break;
+                case 'album' :
+                    if (modState === true)
+                        history.pushState(myRule, null, '#!album');
+                    $("#container-all-section").data("params", myRule);
+                    $("#container-all-section").load('list-album.html');
+                    switchMenu('album');
+                    $('#menu').removeClass('visibile');
+                    break;
 
-            case 'thumbalbum' :
-                if (modState === true)
-                    history.pushState(myRule, null, '#!album-thumb');
-                $("#container-all-section").data("params", myRule);
-                $("#container-all-section").load('thumb-album.html');
-                switchMenu('album');
-                $('#menu').removeClass('visibile');
-                break;
+                case 'thumbalbum' :
+                    if (modState === true)
+                        history.pushState(myRule, null, '#!album-thumb');
+                    $("#container-all-section").data("params", myRule);
+                    $("#container-all-section").load('thumb-album.html');
+                    switchMenu('album');
+                    $('#menu').removeClass('visibile');
+                    break;
 
-            case 'single-album' :
-                if (modState === true)
-                    history.pushState(myRule, null, '#!single-album');
-                $("#container-all-section").data("params", myRule);
-                $("#container-all-section").load('single-album.html');
-                switchMenu('album');
-                $('#menu').removeClass('visibile');
-                break;
+                case 'single-album' :
+                    if (modState === true)
+                        history.pushState(myRule, null, '#!single-album');
+                    $("#container-all-section").data("params", myRule);
+                    $("#container-all-section").load('single-album.html');
+                    switchMenu('album');
+                    $('#menu').removeClass('visibile');
+                    break;
 
 
-            default:
-                if (modState === true)
-                    history.replaceState({request: "home"}, null, '#!home');
-                $('#menu').addClass('visibile');
-                break;
+                default:
+                    if (modState === true)
+                        history.replaceState({request: "home"}, null, '#!home');
+                    $('#menu').addClass('visibile');
+                    break;
+            }
         }
     }
 };
@@ -106,7 +108,7 @@ function switchMenu(voice) {
 }
 
 window.onpopstate = function (event) {
-    console.log(JSON.stringify(event.state));
+    //console.log(JSON.stringify(event.state));
     router.parseRule('<a data-request=\'' + JSON.stringify(event.state) + '\'>', false);
     //window.history.back();
 };
